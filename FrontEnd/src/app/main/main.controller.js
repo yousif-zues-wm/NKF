3e8754e053623ab24a26c4d4f03e2bb309d7cb04
Paul <inbox@paulgg.com>
2017-10-21 21:20:51 -0700

    bootstrap 4 and new html

--- FrontEnd/src/app/main/main.controller.js
+++ FrontEnd/src/app/main/main.controller.js
@@ -6,34 +6,41 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($scope) {
     var vm = this;
+    var hidemF;
+    var derpF;
+    var schol;
+    var carS;
+    var dentalS;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1508612751081;
-    vm.showToastr = showToastr;
 
-    activate();
+    $scope.derpF = false
+    $scope.hidemF = false
+    $scope.carS = false
+    $scope.dentalS = false
+    $scope.schol = false
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    $scope.show = function(){
+       $scope.hidemF = hidemF == true ? hidemF = false : hidemF = true
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
+    $scope.derp = function(){
+      $scope.hidemF = hidemF == true ? hidemF = false : hidemF = true
+      $scope.derpF = derpF == true ? derpF = false : derpF = true
+    }
+    $scope.scholShow = function(){
+      $scope.hidemF = hidemF == true ? hidemF = false : hidemF = true
+      $scope.schol = schol == true ? schol = false : schol = true
+    }
+    $scope.carShow = function(){
+      $scope.hidemF = hidemF == true ? hidemF = false : hidemF = true
+      $scope.carS = carS == true ? carS = false : carS = true
+    }
+    $scope.dentalShow = function(){
+      $scope.hidemF = hidemF == true ? hidemF = false : hidemF = true
+      $scope.dentalS = dentalS == true ? dentalS = false : dentalS = true
     }
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
